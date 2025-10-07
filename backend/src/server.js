d3cc8aad9e22c83c71b43eb03f8ad108cb9426df
Timi <ktimijohn123@gmail.com>
2025-10-07 14:09:14 +0100

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import rateLimiter from "./Middlewares/rateLimiter.js";
 import noteRoutes from "./routes/noteRoutes.js";
@@ -10,13 +11,15 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
-
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 app.use(express.json());
 app.use(rateLimiter);
 
@@ -26,6 +29,13 @@ connectDb().then(() => {
     console.log("server started on port", PORT);
   });
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.use((err, req, res, next) => {
   console.error("Unhandled error:", err);
